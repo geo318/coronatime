4fdf495c16895b9853d02539ebc3a7f1cc368089
geo318 <geo.lomidze@gmail.com>
2022-11-30 23:51:27 +0400

    feat: number format

--- resources/views/components/admin/card.blade.php
+++ resources/views/components/admin/card.blade.php
@@ -6,5 +6,5 @@
         <img src="{{ $icon }}" alt="new cases" class="max-w-[5.625rem] h-16">
     </div>
     <p class="leading-2xl text-xl mb-4">{{ $text }}</p>
-    <p class="text-[2.44rem] leading-[2.94rem] font-black {{ $color }}">{{ $number }}</p>
+    <p class="text-[2.44rem] leading-[2.94rem] font-black {{ $color }}">{{ number_format($number) }}</p>
 </div>
