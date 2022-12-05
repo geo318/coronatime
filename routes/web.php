48145d550b536f77172e647fdc84c48f7ee47b35
geo318 <geo.lomidze@gmail.com>
2022-12-05 11:24:19 +0400

    refactor: stringified controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConfirmEmailController;
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ResetPasswordController;
@@ -10,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['verify' => true]);
 
-Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLanguage']);
+Route::get('lang/{lang}', [LanguageController::class, 'switchLanguage'])->name('lang.switch');
 
 Route::middleware('guest')->group(function () {
 	Route::redirect('/', '/login');
